059bc0534a6e587e72897633105dbfe81edcb0f6
OlegZavadski <z.oleg.f@yandex.ru>
2022-06-24 20:49:17 +0300

    Homework22. Servlet and Postman

--- homework22_Servlet/src/main/java/com/tms/servlet/MyServlet.java
+++ homework22_Servlet/src/main/java/com/tms/servlet/MyServlet.java
@@ -1,9 +1,7 @@
 package com.tms.servlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
